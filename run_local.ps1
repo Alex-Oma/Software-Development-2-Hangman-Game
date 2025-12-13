d11df5f035451dca3dd44f02786799fd08602de0
Alex <2703oleksandr@gmail.com>
2025-12-13 11:30:17 +0000

    1. polished index.html page and fetched all JavaScript code into index.js 2. fixed register.html to lead to login.html when 'Sign In' button is clicked.

--- run_local.ps1
+++ run_local.ps1
@@ -1,2 +1,2 @@
 # This PowerShell helper script so that I can run the FastAPI app locally
-python -m uvicorn backend.app.main:app --reload --port 8000
+python -m uvicorn backend.app.main:app --reload --host 0.0.0.0 --port 8000
